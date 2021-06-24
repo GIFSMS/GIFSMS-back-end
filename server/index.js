cceab957131e1324f70357cb7261338632a67d14
Jarrell28 <jhouston2882@yahoo.com>
2021-06-24 18:32:18 -0500

    latest

--- server/index.js
+++ server/index.js
@@ -30,29 +30,40 @@ db.once('open', function () {
 //Holds participants in each room
 const gifs = io.of('/gifs');
 const gifsRooms = {};
+let allWhoEnter = [];
 
 gifs.on('connection', socket => {
 
     //Function to have users join rooms
     socket.on('join', payload => {
         console.log("Socket:", socket.id);
+
+        //Checks for duplicates in allWhoEnter Array
+        let allWhoEnterDuplicate = allWhoEnter.reduce((acc, userObj) => {
+            if (userObj.nickname === payload.user.nickname) acc = true;
+            return acc;
+        }, false)
+        //Checks if there are no and pushes user and socket id in array
+        if (!allWhoEnterDuplicate) {
+            allWhoEnter.push({ ...payload.user, socketId: socket.id, room: payload.room });
+        }
+        gifs.emit('get all participants', allWhoEnter);
+
         //Initiates list of participants in specific rooms
         //If the room exists, push the joining user in the room array
         if (payload.room in gifsRooms) {
             //Checks for duplicate names in rooms
-            if (!gifsRooms[payload.room].includes(payload.user)) {
-                gifsRooms[payload.room].push(payload.user);
+            let roomDuplicate = gifsRooms[payload.room].reduce((acc, userObj) => {
+                if (userObj.nickname === payload.user.nickname) acc = true;
+                return acc;
+            }, false)
+            if (!roomDuplicate) {
+                gifsRooms[payload.room].push({ ...payload.user, socketId: socket.id });
             }
         } else {
             //The room does not exist, create a new room array and add the user
-            gifsRooms[payload.room] = [payload.user];
+            gifsRooms[payload.room] = [{ ...payload.user, socketId: socket.id }];
         }
-
-        console.log('Room: ', payload.room)
-        console.log('User Joined: ', payload.user);
-
-        console.log(`gifRooms: ${payload.room}`, gifsRooms[payload.room]);
-        console.log('gifRooms: all', gifsRooms);
 
         //Joins the user to the room
         socket.join(payload.room);
@@ -67,18 +78,18 @@ gifs.on('connection', socket => {
         //Get List of rooms and send to all clients
         let rooms = Object.keys(gifsRooms);
         gifs.emit('get rooms', { rooms });
-
     });
 
     socket.on('logingif', payload => {
         let user = payload.user
         console.log("IN LOGIN: ", payload)
-        User.find({ email: user }, function (err, userer) {
+        User.find({ email: user.email }, function (err, userer) {
             if (err) { return console.error(err) }
             console.log("USERER: ", userer)
             if (!userer[0]) {
                 const newUser = new User({
-                    email: user,
+                    email: user.email,
+                    nickname: user.nickname,
                     favorites: [],
                     friends: [],
                     created: new Date(),
@@ -109,6 +120,14 @@ gifs.on('connection', socket => {
 
     })
 
+    socket.on('fetch profile', payload => {
+        User.find({ email: payload }, function (err, selected) {
+            if (err) { return console.error(err) }
+            console.log('Fetched Profile:', selected[0]);
+            socket.emit('fetch profile', selected[0]);
+        })
+    })
+
 
     //listen to new messages from clients
     socket.on('message', payload => {
@@ -121,7 +140,12 @@ gifs.on('connection', socket => {
     socket.on('leave', payload => {
 
         //Removes leaving user from room array
-        gifsRooms[payload.room] = gifsRooms[payload.room].filter(user => user !== payload.user);
+        gifsRooms[payload.room] = gifsRooms[payload.room].filter(user => user.nickname !== payload.user.nickname);
+
+        //Removes leaving user from all who enter
+        allWhoEnter = allWhoEnter.filter(user => user.nickname !== payload.user.nickname);
+        //Sends all participants to users
+        gifs.emit('get all participants', allWhoEnter);
 
         //Sends participants to all clients in specific room
         let participants = gifsRooms[payload.room];
@@ -144,6 +168,38 @@ gifs.on('connection', socket => {
         //Handles removal of user from from
         socket.leave(payload.room);
     });
+
+    socket.on('disconnect', payload => {
+        console.log('SocketID: ', socket.id);
+
+        let user = allWhoEnter.filter(userObj => userObj.socketId === socket.id)[0]
+        if (user) {
+            //Removes leaving user from all who enter
+            allWhoEnter = allWhoEnter.filter(user => user.socketId !== socket.id);
+            //Sends all participants to users
+            gifs.emit('get all participants', allWhoEnter);
+            console.log('allWhoEnter', allWhoEnter)
+
+            //Sends participants to all clients in specific room
+            let participants = gifsRooms[user.room].filter(user => user.socketId !== socket.id);
+            gifs.in(user.room).emit('get participants', { participants });
+
+            //Emits user left room notification to clients in specific room
+            socket.to(user.room).emit('user disconnected', { user });
+
+            //When a user leaves a room, see if room is empty and delete room
+            if (gifsRooms[user.room].length === 0) {
+                if (user.room !== "Main Room") {
+                    delete gifsRooms[user.room];
+                }
+            }
+
+            //Get List of rooms and send to all clients
+            let rooms = Object.keys(gifsRooms);
+            gifs.emit('get rooms', { rooms });
+        }
+
+    })
 })
 // otherApp.listen(PORTER, () => console.log(`listening on ${PORTER}`));
 server.listen(3004, () => console.log(`listening on ${3004}`));
